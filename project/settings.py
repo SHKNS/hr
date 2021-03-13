9dbc35b0e42cf631824348e994bbd86febb2717d
SHKNS <jaeje514@gmail.com>
2021-03-13 16:20:20 +0900

    'd'

--- project/settings.py
+++ project/settings.py
@@ -15,7 +15,6 @@ import os
 import dj_database_url
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
