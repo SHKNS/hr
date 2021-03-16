14fecc0bc52d1227d698ad7906c225a74e8cd211
SHKNS <80527161+SHKNS@users.noreply.github.com>
2021-03-16 14:44:35 +0900

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
